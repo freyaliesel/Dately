a2dc01ac605a6b193013f4ac840cd3812408372d
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-03-10 19:53:12 -0600

    adjusted display

--- places.js
+++ places.js
@@ -1,3 +1,5 @@
+// figuring out google maps stuff
+
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
@@ -6,16 +8,16 @@ let service;
 let infowindow;
 
 function initMap() {
-    const sydney = new google.maps.LatLng(41.90362, -87.67588);
+    const location = new google.maps.LatLng(41.90362, -87.67588);
 
     infowindow = new google.maps.InfoWindow();
     map = new google.maps.Map(document.getElementById("map"), {
-        center: sydney,
-        zoom: 15,
+        center: location,
+        zoom: 16,
     });
 
     const request = {
-        query: "Wicker Park",
+        query: "Empty Bottle",
         fields: ["name", "geometry"],
     };
 
@@ -46,12 +48,10 @@ function createMarker(place) {
     });
 }
 
-
-
-
+// figuring out Yelp stuff
 
 var date = dayjs().unix();
-var place = "chicago"
+var place = "logansquare";
 
 // use cors-anywhere to access yelp API
 // accepts 2 variables as input: locaiton and date in unix
@@ -60,7 +60,9 @@ function accessYelp(place, date) {
         "https://cors-anywhere-bc.herokuapp.com/https://api.yelp.com/v3/events?";
     let location = "location=" + place;
     let startDate = "&start_date=" + date;
-    fetch(url + location + startDate, {
+    let results = "&limit=10";
+    let excluded = "&excluded_events=chicago-chicago-bachelor-party-exotic-dancers-topless-nudy-waitresses-call-us-312-488-4673"
+    fetch(url + location + startDate + results + excluded, {
         method: "get",
         headers: new Headers({
             Authorization:
@@ -68,7 +70,66 @@ function accessYelp(place, date) {
         }),
     })
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => parseResults(data.events));
 }
 
 accessYelp(place, date);
+
+function parseResults(events) {
+    console.log("displaying results");
+    console.log(events);
+    let displayEl = document.getElementById("results");
+
+    events.forEach((event) => {
+        let imgEl = document.createElement("img");
+        displayEl.appendChild(imgEl);
+        imgEl.setAttribute("src", event.image_url);
+        imgEl.style.maxHeight = "300px";
+        imgEl.style.maxWidth = "500px";
+
+        let nameEl = document.createElement("ul");
+        let li = document.createElement("li");
+        displayEl.appendChild(nameEl);
+        nameEl.appendChild(li);
+        li.textContent = event.name;
+
+        li = document.createElement("li");
+        nameEl.appendChild(li);
+        li.textContent = event.category;
+
+        li = document.createElement("li");
+        nameEl.appendChild(li);
+        li.textContent = event.description;
+
+        li = document.createElement("li");
+        nameEl.appendChild(li);
+        li.textContent = "$" + event.cost;
+
+        li = document.createElement("li");
+        nameEl.appendChild(li);
+        let a = document.createElement("a");
+        li.appendChild(a);
+        a.setAttribute("href", event.event_site_url);
+        a.textContent = event.name;
+
+        li = document.createElement("li");
+        nameEl.appendChild(li);
+        li.textContent = "location: " + event.location.address1;
+
+        li = document.createElement("li");
+        nameEl.appendChild(li);
+        li.textContent = `coordinates: ${event.latitude}, ${event.longitude}`;
+
+        li = document.createElement("li");
+        nameEl.appendChild(li);
+        a = document.createElement("a");
+        li.appendChild(a);
+        a.setAttribute("href", event.tickets_url);
+        a.textContent = "Link to Yelp Event";
+
+        li = document.createElement("li");
+        nameEl.appendChild(li);
+        li.textContent = dayjs(event.time_start).format("ddd, MMM D, h:mma");
+        // dayjs.utc(event.time_start).local().format("ddd, MMM D, h:mma")
+    });
+}
