e23c76e2b029d30242548b16c05943393ddf53f6
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-03-10 21:02:00 -0600

    adjust search box, accessed information, save info

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,47 @@
 // set global variables to empty strings
-var userDate = "";
-var userLocation = "";
+var date = "";
 
 // event listeners
 document.addEventListener("DOMContentLoaded", function () {
-  // modal section
-  var elems = document.querySelectorAll(".modal");
-  var instances = M.Modal.init(elems);
+    // modal section
+    var elems = document.querySelectorAll(".modal");
+    var instances = M.Modal.init(elems);
 
-  // datepicker section
-  // jquery is the only option here due to how datepicker is written in materialize
-  // when user picks a date in calendar, it stores date to 'userDate' variable in format yyyy-mm-dd
-  var elems = document.querySelectorAll(".datepicker");
-  var instances = M.Datepicker.init(elems);
-  $("#datepicker").datepicker({
-    onSelect: function (date) {
-      userDate = date.toISOString().substring(0, 10);
-      console.log("The value of userDate is: " + userDate);
-      document.querySelector('input').value = userDate;
-    },
-  });
+    // datepicker section
+    // jquery is the only option here due to how datepicker is written in materialize
+    // when user picks a date in calendar, it stores date to 'userDate' variable in format yyyy-mm-dd
+    var elems = document.querySelectorAll(".datepicker");
+    var instances = M.Datepicker.init(elems);
+    $("#datepicker").datepicker({
+        onSelect: function (input) {
+            date = input.toISOString().substring(0, 10);
+            console.log("The value of userDate is: " + date);
+            document.querySelector("input").value = date;
+        },
+    });
 
-  // dropdown section
-  // when user picks a location from dropdown, it stores location in 'userLocation' variable
-  var dropSelect = document.getElementById("dropdown").addEventListener('change', function() {
-    userLocation = this.value;
-    console.log("The value of userLocation is: " + userLocation);
-  });
+    // dropdown section
+    // when user picks a location from dropdown, it stores location in 'userLocation' variable
+    // var dropSelect = document.getElementById("dropdown").addEventListener('change', function() {
+    //   userLocation = this.value;
+    //   console.log("The value of userLocation is: " + userLocation);
+    // });
 });
+
+document
+    .getElementById("submit-btn")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log(this);
+        let location = document.getElementById("location").value;
+        console.log(date + "," + location);
+
+        let parameters = {
+          date: date,
+          location: location
+        }
+
+        localStorage.setItem("searchParameters", JSON.stringify(parameters));
+
+        window.location.assign("./search_index.html");
+    });
