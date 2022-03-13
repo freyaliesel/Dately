e7fcbb9f957efa96f9715499071986468c9e7368
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-03-12 19:16:43 -0600

    restaurant details only populate once

--- assets/search_results.js
+++ assets/search_results.js
@@ -1,10 +1,10 @@
 // implementing Yelp API
 
 function retrieveLocation() {
-  let searchParameters = JSON.parse(localStorage.getItem("searchParameters"));
-  searchParameters.date = dayjs(searchParameters.date).unix();
-  searchParameters.location = searchParameters.location.replace(/\s/g, "");
-  return searchParameters;
+    let searchParameters = JSON.parse(localStorage.getItem("searchParameters"));
+    searchParameters.date = dayjs(searchParameters.date).unix();
+    searchParameters.location = searchParameters.location.replace(/\s/g, "");
+    return searchParameters;
 }
 // console.log(retrieveLocation());
 
@@ -130,7 +130,8 @@ function generateEventResults(data) {
 
         buttonEl = document.createElement("button");
         dEl.appendChild(buttonEl);
-        buttonEl.className = "waves-effect waves-light white-text btn-flat pink center-align";
+        buttonEl.className =
+            "waves-effect waves-light white-text btn-flat pink center-align";
         buttonEl.textContent = "Find Restaurants";
         iEl = document.createElement("i");
         iEl.className = "material-icons left bucketlist-add";
@@ -187,212 +188,222 @@ document
         }
     });
 
-
 // google api
 const placeArray = new Array();
 
 function callbackOne(request, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK && request) {
-    console.log("processing places");
-    localStorage.setItem("places", JSON.stringify(request));
-    generatePlaceResults();
-  }
+    if (status === google.maps.places.PlacesServiceStatus.OK && request) {
+        console.log("processing places");
+        localStorage.setItem("places", JSON.stringify(request));
+        generatePlaceResults();
+    }
 }
 
 function callbackTwo(placeDetails, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    generatePlaceDetails(placeDetails);
-  }
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        generatePlaceDetails(placeDetails);
+    }
 }
 
 function generatePlaceDetails(data) {
-  console.log(data);
-  let index = localStorage.getItem("resCardIndex");
+    console.log(data);
+    let index = localStorage.getItem("resCardIndex");
 
-  let parentContainer = document.querySelector("#google-results");
+    let parentContainer = document.querySelector("#google-results");
 
-  let cards = parentContainer.children;
-  //console.log(cards);
-  
- //console.log(cards[index]);
-  
- let reveal = cards[index].children[3];
+    let cards = parentContainer.children;
+    //console.log(cards);
 
-    // restaurant name
-    let spanEl = document.createElement("span");
-    spanEl.className = "card-title";
-    let iEl = document.createElement("i");
-    spanEl.appendChild(iEl);
-    iEl.className = "material-icons right";
-    iEl.textContent = "close";
-    iEl.style.color = "black";
-    reveal.appendChild(spanEl);
-    spanEl.textContent = data.name;
+    //console.log(cards[index]);
 
-    let pEl = document.createElement("p");
-    reveal.appendChild(pEl);
-    // pEl.textContent = data.opening_hours;
-    // figure out how to display all the closing and opening hours for each day
-    //console.log(data.opening_hours);
+    let reveal = cards[index].children[3];
 
-    let aEl = document.createElement("a");
-    reveal.appendChild(aEl);
-    aEl.setAttribute("href", "tel:"+data.international_phone_number);
-    aEl.textContent = data.formatted_phone_number;
+    let cardEls = reveal.children;
 
-    let brEl = document.createElement("br");
-    reveal.appendChild(brEl);
+    if (cardEls.length == 0) {
+        // restaurant name
+        let spanEl = document.createElement("span");
+        spanEl.className = "card-title";
+        let iEl = document.createElement("i");
+        spanEl.appendChild(iEl);
+        iEl.className = "material-icons right";
+        iEl.textContent = "close";
+        iEl.style.color = "black";
+        reveal.appendChild(spanEl);
+        spanEl.textContent = data.name;
 
-    aEl = document.createElement("a");
-    reveal.appendChild(aEl);
-    aEl.setAttribute("href", data.website);
-    aEl.textContent = "Website";
+        let pEl = document.createElement("p");
+        reveal.appendChild(pEl);
+        // pEl.textContent = data.opening_hours;
+        // figure out how to display all the closing and opening hours for each day
+        //console.log(data.opening_hours);
+
+        let aEl = document.createElement("a");
+        reveal.appendChild(aEl);
+        aEl.setAttribute("href", "tel:" + data.international_phone_number);
+        aEl.textContent = data.formatted_phone_number;
+
+        let brEl = document.createElement("br");
+        reveal.appendChild(brEl);
+
+        aEl = document.createElement("a");
+        reveal.appendChild(aEl);
+        aEl.setAttribute("href", data.website);
+        aEl.textContent = "Website";
+    }
 }
 
 function generatePlaceResults() {
-  console.log("displaying places");
-  let places = JSON.parse(localStorage.getItem("places"));
-  console.log(places);
-  let displayEl = document.getElementById("google-results");
-  let i = 0;
+    console.log("displaying places");
+    let places = JSON.parse(localStorage.getItem("places"));
+    console.log(places);
+    let displayEl = document.getElementById("google-results");
+    let i = 0;
 
-  places.forEach((place) => {
-    //create card
-    //add logic to not display closed businesses, but keep increasing the index of the array goes up
-    let cardEl = document.createElement("div");
-    cardEl.className = "card sticky-action event";
-    cardEl.id = `card-${i}`;
-    displayEl.appendChild(cardEl);
-    //create array for cardEl.id and corresponding place_id
-    placeArray.push(place.place_id);
-    i++;
+    places.forEach((place) => {
+        //create card
+        //add logic to not display closed businesses, but keep increasing the index of the array goes up
+        let cardEl = document.createElement("div");
+        cardEl.className = "card sticky-action event";
+        cardEl.id = `card-${i}`;
+        displayEl.appendChild(cardEl);
+        //create array for cardEl.id and corresponding place_id
+        placeArray.push(place.place_id);
+        i++;
 
-    // div for image
-    let divEl = document.createElement("div");
-    divEl.className = "card-image";
-    cardEl.appendChild(divEl);
+        // div for image
+        let divEl = document.createElement("div");
+        divEl.className = "card-image";
+        cardEl.appendChild(divEl);
 
-    // image
-    let imgEl = document.createElement("img");
-    imgEl.className = "activator";
-    imgEl.setAttribute("src", "./assets/links/drinks.jpg");
-    divEl.appendChild(imgEl);
+        // image
+        let imgEl = document.createElement("img");
+        imgEl.className = "activator";
+        imgEl.setAttribute("src", "./assets/links/drinks.jpg");
+        divEl.appendChild(imgEl);
 
-    // add button
-    let buttonEl = document.createElement("button");
-    buttonEl.className =
-      "btn-floating halfway-fab waves-effect waves-light pink";
-    divEl.appendChild(buttonEl);
-    let iEl = document.createElement("i");
-    iEl.className = "material-icons";
-    iEl.textContent = "add";
-    buttonEl.appendChild(iEl);
+        // add button
+        let buttonEl = document.createElement("button");
+        buttonEl.className =
+            "btn-floating halfway-fab waves-effect waves-light pink";
+        divEl.appendChild(buttonEl);
+        let iEl = document.createElement("i");
+        iEl.className = "material-icons";
+        iEl.textContent = "add";
+        buttonEl.appendChild(iEl);
 
-    // create div for content
-    divEl = document.createElement("div");
-    cardEl.appendChild(divEl);
-    divEl.className = "card-content";
+        // create div for content
+        divEl = document.createElement("div");
+        cardEl.appendChild(divEl);
+        divEl.className = "card-content";
 
-    // place name
-    let spanEl = document.createElement("span");
-    spanEl.className = "card-title activator";
-    spanEl.textContent = place.name;
-    divEl.appendChild(spanEl);
+        // place name
+        let spanEl = document.createElement("span");
+        spanEl.className = "card-title activator";
+        spanEl.textContent = place.name;
+        divEl.appendChild(spanEl);
 
-    // place address
-    pEl = document.createElement("p");
-    divEl.appendChild(pEl);
-    pEl.textContent = place.formatted_address;
+        // place address
+        pEl = document.createElement("p");
+        divEl.appendChild(pEl);
+        pEl.textContent = place.formatted_address;
 
-    // rating
-    pEl = document.createElement("p");
-    divEl.appendChild(pEl);
-    place.rating !== 0
-      ? (pEl.textContent = "Rating: " + place.rating + "/5")
-      : (pEl.textContent = "No Rating");
+        // rating
+        pEl = document.createElement("p");
+        divEl.appendChild(pEl);
+        place.rating !== 0
+            ? (pEl.textContent = "Rating: " + place.rating + "/5")
+            : (pEl.textContent = "No Rating");
 
-    // create div for links
-    divEl = document.createElement("div");
-    divEl.className = "card-action";
-    cardEl.appendChild(divEl);
+        // create div for links
+        divEl = document.createElement("div");
+        divEl.className = "card-action";
+        cardEl.appendChild(divEl);
 
-    // create div for reveal
-    divEl = document.createElement("div");
-    divEl.className = "card-reveal";
-    cardEl.appendChild(divEl);
+        // create div for reveal
+        divEl = document.createElement("div");
+        divEl.className = "card-reveal";
+        cardEl.appendChild(divEl);
 
-    // // place name
-    // spanEl = document.createElement("span");
-    // spanEl.className = "card-title activator";
-    // spanEl.textContent = place.name;
-    // divEl.appendChild(spanEl);
+        // // place name
+        // spanEl = document.createElement("span");
+        // spanEl.className = "card-title activator";
+        // spanEl.textContent = place.name;
+        // divEl.appendChild(spanEl);
 
-    // place type
-    // pEl = document.createElement("p");
-    // divEl.appendChild(pEl);
-    // let placeType = "";
-    // for (i = 0; i < place.types.length; i++) {
-    //   placeType = placeType + " " + place.types[i];
-    //   i++;
-    // }
-    // pEl.textContent = "Category: " + placeType + ", ";
-  });
+        // place type
+        // pEl = document.createElement("p");
+        // divEl.appendChild(pEl);
+        // let placeType = "";
+        // for (i = 0; i < place.types.length; i++) {
+        //   placeType = placeType + " " + place.types[i];
+        //   i++;
+        // }
+        // pEl.textContent = "Category: " + placeType + ", ";
+    });
 }
 
-function nextSearch (event) {
-  let current = event.target;
-  let card = current.closest(".event");
-  console.log(card);
-  let index = card.getAttribute("id");
-  index = index.substring(index.indexOf("-") + 1);
-  localStorage.setItem("resCardIndex", index);
-  let passId = placeArray[index];
-  getPlaceInfo(passId);
+function nextSearch(event) {
+    let current = event.target;
+    let card = current.closest(".event");
+    console.log(card);
+    let index = card.getAttribute("id");
+    index = index.substring(index.indexOf("-") + 1);
+    localStorage.setItem("resCardIndex", index);
+    let passId = placeArray[index];
+    getPlaceInfo(passId);
 }
 
 // listener for user click on a place card
 document
-  .getElementById("google-results")
-  .addEventListener("click", function (event) {
-    if (event.target.className.includes("activator")) {
-      console.log("card clicked");
-      nextSearch(event);
-    }
-  });
+    .getElementById("google-results")
+    .addEventListener("click", function (event) {
+        if (event.target.className.includes("activator")) {
+            console.log("card clicked");
+            nextSearch(event);
+        }
+    });
 
 // kicks off when user clicks a button for an event
 function initSearch(coords) {
-  let service;
-  var elem = document.querySelector("#google-results");
+    let service;
+    var elem = document.querySelector("#google-results");
 
-  const location = {
-    lat: coords.latitude,
-    lng: coords.longitude,
-  };
+    const location = {
+        lat: coords.latitude,
+        lng: coords.longitude,
+    };
 
-  var request = {
-    location: location,
-    radius: "100",
-    type: ["restaurant"],
-  };
-  service = new google.maps.places.PlacesService(elem);
-  service.textSearch(request, callbackOne);
+    var request = {
+        location: location,
+        radius: "100",
+        type: ["restaurant"],
+    };
+    service = new google.maps.places.PlacesService(elem);
+    service.textSearch(request, callbackOne);
 }
 
 function getPlaceInfo(passId) {
-  let service;
-  var elem = document.querySelector("#empty");
-  console.log("getting place details from place_id");
+    let service;
+    var elem = document.querySelector("#empty");
+    console.log("getting place details from place_id");
 
-  // get place_id for the specific card user clicked
-  let placeId = passId;
-  console.log(placeId);
+    // get place_id for the specific card user clicked
+    let placeId = passId;
+    console.log(placeId);
 
-  var placeRequest = {
-    placeId: placeId,
-    fields: ["name", "formatted_phone_number", "photos", "website", "international_phone_number", "opening_hours"],
-  };
+    var placeRequest = {
+        placeId: placeId,
+        fields: [
+            "name",
+            "formatted_phone_number",
+            "photos",
+            "website",
+            "international_phone_number",
+            "opening_hours",
+        ],
+    };
 
-  service = new google.maps.places.PlacesService(elem);
-  service.getDetails(placeRequest, callbackTwo);
+    service = new google.maps.places.PlacesService(elem);
+    service.getDetails(placeRequest, callbackTwo);
 }
