eb7e80166b5c6389c217e0ec95f8144f9da8d048
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-03-13 11:40:43 -0500

    changed to auto initialize materialize components

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,14 @@ var date = "";
 // event listeners
 document.addEventListener("DOMContentLoaded", function () {
     // modal section
-    var elems = document.querySelectorAll(".modal");
-    var instances = M.Modal.init(elems);
-
+    // var elems = document.querySelectorAll(".modal");
+    // var instances = M.Modal.init(elems);
+    M.AutoInit();
     // datepicker section
     // jquery is the only option here due to how datepicker is written in materialize
     // when user picks a date in calendar, it stores date to 'userDate' variable in format yyyy-mm-dd
-    var elems = document.querySelectorAll(".datepicker");
-    instances = M.Datepicker.init(elems);
+    // var elems = document.querySelectorAll(".datepicker");
+    // instances = M.Datepicker.init(elems);
     $("#datepicker").datepicker({
         onSelect: function (input) {
             date = input.toISOString().substring(0, 10);
